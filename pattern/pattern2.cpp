a67e53c9917e73f3ebd3f292b60b442f1104fb22
creativeSandeepKumar <sdp4209@gmail.com>
2025-07-19 14:04:09 +0530

    pattern question completed

--- pattern/pattern2.cpp
+++ pattern/pattern2.cpp
@@ -6,6 +6,8 @@ void printAlphabetTrianle();
 void printReverseAlphabetTrianle();
 void printInvertedSapceAlphabetTrianle();
 void printReverseAngleTrianle();
+void NumberPyramid();
+void dabanggPattern();
 
 
 int main(){
@@ -13,7 +15,9 @@ int main(){
     // printAlphabetTrianle();
     // printReverseAlphabetTrianle();
     // printInvertedSapceAlphabetTrianle();
-    printReverseAngleTrianle();
+    // printReverseAngleTrianle();
+    // NumberPyramid();
+    dabanggPattern();
     return 0;
 }
 
@@ -81,4 +85,40 @@ void printReverseAngleTrianle(){
         cout << endl;
     }
     cout << endl;
+}
+
+void NumberPyramid () {
+    int height = 5;
+    for(int i = 1; i <= height; i++){
+        for(int k = 1; k <= (height - i); k++){
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++){
+            cout << j;
+        }
+        for(int l = i - 1; l >= 1; l--){
+            cout << l;
+        }
+        cout << endl;
+    }
+}
+
+void dabanggPattern(){
+    int h = 5;
+    for(int i = 1; i <= h; i++){
+        for(int j = 1; j <=( h - i + 1); j++){
+            cout << j << " ";
+        }
+        for(int k = 1; k <=( i); k++){
+            cout << "* ";
+        }
+        for(int k = (i - 1); k >= 1; k--){
+            cout << "* ";
+        }
+
+        for(int l = (h - i + 1); l >= 1; l--){
+            cout << l << " ";
+        }
+        cout << endl;
+    }
 }
