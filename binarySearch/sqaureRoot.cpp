4b25b711aafd62fd3db0b2964e336a7ecc9c214f
creativeSandeepKumar <sdp4209@gmail.com>
2025-07-24 00:25:00 +0530

    square toot

---
+++ binarySearch/sqaureRoot.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+
+long long int sqrtInteger(int n){
+     int s = 0;
+     int e = n - 1;
+    long long int mid = s + (e - s)/2;
+    long long ans = -1;
+
+    while(s <= e){
+        long long int square = mid * mid;
+        if(square == n) return mid;
+
+        if(square < n){
+            ans = mid;
+            s = mid + 1;
+        } else {
+            e = mid  - 1;
+        }
+        mid = s + (e - s)/2;
+    }
+    return ans;
+}
+
+double morePrecision(int n, int precsion, int tempSol){
+    double factorial = 1;
+    double ans = tempSol;
+    for(int i = 0; i < precsion; i++){
+        factorial = factorial/10;
+        for(int j = ans; j * j < n; j = j + factorial){
+            ans = j;
+        }
+    }
+
+    return ans;
+}
+
+int main(){
+    int n = 50;
+    int tempSol = sqrtInteger(n);
+    cout << "The square root of " << n << " is: " << morePrecision(n, 3, tempSol) << endl;
+    return 0;
+}
