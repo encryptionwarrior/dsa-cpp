9d1254386380f223030d4105c72cae3a259b6083
creativeSandeepKumar <sdp4209@gmail.com>
2025-08-05 02:11:15 +0530

    linked list is going on

---
+++ linked-list/intro.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public: 
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+
+int main(){
+
+    Node head(10);
+    Node head2(20);
+    head.next = &head2;
+
+    cout << head.data << " & " << head.next->data << endl;
+
+    return 0;
+}
