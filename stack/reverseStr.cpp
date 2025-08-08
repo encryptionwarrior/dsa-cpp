83492145dbfd5e359441e384d2f5b2e79f130aa0
creativeSandeepKumar <sdp4209@gmail.com>
2025-08-09 01:20:52 +0530

    stack is going on

--- stack/reverseStr.cpp
+++ stack/reverseStr.cpp
@@ -22,7 +22,137 @@ void reverseStr() {
     cout << "Original string : " << str << endl;
     cout << "Reversed string : " << answer << endl;
 }
+
+void deleteMiddle(stack<int> &st, int count, int size){
+    if(count == size/2){
+        st.pop();
+        return;
+    }
+
+    int top = st.top();
+    st.pop();
+
+    deleteMiddle(st, count + 1, size);
+
+    st.push(top);
+}
+
+void testDeleteMiddle(){
+    stack<int> st;
+    st.push(5);
+    st.push(5);
+    st.push(5);
+    st.push(5);
+    st.push(5);
+    st.push(5);
+
+    int count = 0 ;
+    int n = st.size();
+
+    cout << "Size of stack : " << st.size() << endl;
+
+    deleteMiddle(st, count, n);
+
+    cout << "Size of stack : " << st.size() << endl;
+}
+
+bool matches(char a, char b){
+    switch(a){
+        case ')':
+            return (b == '(') ? true : false;
+        case '}':
+            return (b == '{') ? true : false;
+        case ']':
+            return (b == '[') ? true : false;
+        default:
+            return false;
+    }
+}
+
+bool isValid(string str){
+    stack<char> st;
+
+    for(int i = 0; i < str.length(); i++){
+        char ch = str[i];
+
+        if(ch == '(' || ch == '{'  || ch == '['){
+            st.push(ch);
+        } else {
+            if(!st.empty() && matches(ch, st.top())){
+                st.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return (st.empty()) ? true : false;
+}
+
+void validParenthesis(){
+    string str;
+    cout << "Enter the expression : ";
+
+    getline(cin, str);
+
+    if(isValid(str)){
+        cout << "Balanced " << endl;
+    } else {
+        cout << "Not balanced" << endl;
+    }
+}
+
+void inputStack(stack<int> &st){
+    int size;
+    cout << "Enter the size : ";
+    cin >> size;
+
+    cout << "Enter the stack elements : ";
+    for(int i = 0; i < size; i++){
+        int temp;
+        cin >> temp;
+        st.push(temp);
+    }
+}
+
+void printStack(stack<int> st){
+    cout << "Stack : ";
+    while(!st.empty()){
+        cout << st.top();
+        st.pop();
+    }
+    cout << endl;
+}
+
+void insertBottom(stack<int>&st, int data){
+    if(st.empty()){
+        st.push(data);
+        return;
+    }
+
+    int top = st.top();
+    st.pop();
+
+    insertBottom(st, data);
+
+    st.push(top);
+}
+
+
+
+
 int main(){
     reverseStr();
+    testDeleteMiddle();
+    validParenthesis();
+
+    stack<int> st;
+
+    inputStack(st);
+    printStack(st);
+
+    cout << "Inserting 10 at bottom..." << endl;
+    insertBottom(st, 10);
+    printStack(st);
     return 0;
 }
