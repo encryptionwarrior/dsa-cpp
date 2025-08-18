d233376a77f6b03f76cc9ec17059b320b9332e81
creativeSandeepKumar <sdp4209@gmail.com>
2025-08-19 02:24:48 +0530

    reverse arr

--- queue/day01.cpp
+++ queue/day01.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<queue>
 #include<deque>
+#include<stack>
 using namespace std;
 
 void queueBasic(){
@@ -422,11 +423,53 @@ void DequeImplement(){
     d.pop_front();
 }
 
+void Rev_Queue_App1(queue<int> &Q){
+    stack<int> temp;
+
+    while(!Q.empty()){
+        temp.push(Q.front());
+        Q.pop();
+    }
+
+    while(!temp.empty()){
+        Q.push(temp.top());
+        temp.pop();
+    }
+}
+
+void print(queue<int> Q){
+    while(!Q.empty()){
+        cout << Q.front() << " ";
+        Q.pop();
+    }
+
+    cout << endl;
+}
+
+void ReverseQueueimpl(){
+    queue<int>Q;
+
+    Q.push(77);
+    Q.push(65);
+    Q.push(98);
+    Q.push(46);
+    Q.push(32);
+
+    cout << "Before reverse : ";
+    print(Q);
+    Rev_Queue_App1(Q);
+
+    cout << "After reverse : ";
+    print(Q);
+
+}
+
 int main(){
     // queueBasic();
     // manualQueue();
     // CircularQueue();
     // doublyEndedQueue();
-    DequeImplement();
+    // DequeImplement();
+    ReverseQueueimpl();
     return 0;
 }
