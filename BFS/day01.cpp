a38830556eb18522f36982dacb5ec44a7972a5f6
creativeSandeepKumar <sdp4209@gmail.com>
2025-08-23 02:48:30 +0530

    bst search completed

--- BFS/day01.cpp
+++ BFS/day01.cpp
@@ -78,15 +78,86 @@ void levelOrderTraversal(Node* root){
 
 }
 
+void preOrderRecursive(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    cout << root->data << " ";
+    preOrderRecursive(root->left);
+    preOrderRecursive(root->right);
+}
+void inOrderRecursive(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    inOrderRecursive(root->left);
+    cout << root->data << " ";
+    inOrderRecursive(root->right);
+}
+void postOrderRecursive(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    postOrderRecursive(root->left);
+    postOrderRecursive(root->right);
+    cout << root->data << " ";
+}
+
+
+bool searchBST(Node* root, int key){
+    if(root == NULL){
+        return false;
+    }
+
+    if(root->data == key){
+        return true;
+    }
+
+    if(root->data > key){
+        return searchBST(root->right, key);
+    } else {
+        return searchBST(root->left, key);
+    }
+}
+
+
 int main(){
 
     Node* root = NULL;
 
     cout << "--- Creating Binary Search Tree ---" << endl;
     takeInput(root);
+    cout << endl;
 
-    cout << endl << "Level order Traversal of BST: " << endl;
-    levelOrderTraversal(root);
+    // cout << endl << "Level order Traversal of BST: " << endl;
+    // levelOrderTraversal(root);
+    // cout << endl << "In order Traversal : " << endl;
+    // inOrderRecursive(root);
+
+
+    // cout << endl << "pre order Traversal : " << endl;
+    // preOrderRecursive(root);
+
+
+    // cout << endl << "post order Traversal : " << endl;
+    // postOrderRecursive(root);
+
+    int key;
+
+    cout << "Enter the key to search: ";
+    cin >> key;
+
+    bool found = searchBST(root, key);
+
+    if(found){
+        cout << "Key " << key << " is present in BST!" << endl;
+    } else {
+        cout << "Key " << key << " is absent in BST!" << endl;
+    }
+    
 
     cout << endl;
 
