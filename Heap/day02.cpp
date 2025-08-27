79e09097d5a00362c21b2331cb50a713f2e595e7
creativeSandeepKumar <sdp4209@gmail.com>
2025-08-28 02:09:38 +0530

    length of ropes is completed

--- Heap/day02.cpp
+++ Heap/day02.cpp
@@ -271,12 +271,56 @@ void merge2Maxheap(){
     cout << endl;
 }
 
+int minimumCost(vector<int> arr){
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int cost = 0;
+
+    for(auto i: arr){
+        minHeap.push(i);
+    }
+
+    while(minHeap.size() > 1){
+        int a = minHeap.top();
+        minHeap.pop();
+
+
+        int b = minHeap.top();
+        minHeap.pop();
+
+        int sum = a + b;
+
+        cost += sum;
+
+        minHeap.push(sum);
+    }
+
+    return cost;
+}
+
+void minimumCostRope(){
+    vector<int> arr;
+
+    cout << "Enter the lengths of ropes (enter -1 to stop) : ";
+    int temp;
+    cin >> temp;
+
+    while(temp != -1){
+        arr.push_back(temp);
+        cin >> temp;
+    }
+
+    int cost = minimumCost(arr);
+
+    cout << "mimimum cost to join all ropes = " << cost << endl;
+}
+
 int main(){
 
 //   kthSmallestFunction();
 //   kthLargestFunc();
 //   isBinaryTreeHeap();
-merge2Maxheap();
+// merge2Maxheap();
+minimumCostRope();
 
     return 0;
 }
