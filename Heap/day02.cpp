27ffd50353baf0f5935c3778c2ad00d25defb4d4
creativeSandeepKumar <sdp4209@gmail.com>
2025-08-28 00:59:00 +0530

    max largest element is done

--- Heap/day02.cpp
+++ Heap/day02.cpp
@@ -46,9 +46,51 @@ void kthSmallestFunction(){
     cout << "Kth smallest element : " << answer << endl;
 }
 
+
+int kthLargest(vector<int> arr, int k){
+    priority_queue<int, vector<int>, greater<int>> Heap;
+
+    for(int i = 0; i < k; i++){
+        Heap.push(arr[i]);
+    }
+
+    for(int i = k; i < arr.size(); i++){
+        Heap.pop();
+        Heap.push(arr[i]);
+    }
+
+    return Heap.top();
+};
+
+void kthLargestFunc(){
+     vector<int> arr;
+    cout << "Enter the elements of array (enter -1 to stop) : ";
+    int temp;
+    cin >> temp;
+
+    while(temp != -1){
+        arr.push_back(temp);
+        cin >> temp;
+    }
+
+    int k;
+    cout << "Enter the value of k : ";
+    cin >> k;
+
+    if(k <= 0 || k > arr.size()){
+        cout << "Invalid k value. K must be between 1 and " << arr.size() << endl;
+        return ;
+    }
+
+    int answer = kthLargest(arr, k);
+
+    cout << "Kth Largest element : " << answer << endl;
+}
+
 int main(){
 
-  kthSmallestFunction();
+//   kthSmallestFunction();
+  kthLargestFunc();
 
     return 0;
 }
