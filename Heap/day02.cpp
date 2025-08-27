82d5d81584f7b4b6d325835fd35f1840dbe6552e
creativeSandeepKumar <sdp4209@gmail.com>
2025-08-28 00:46:20 +0530

    kth smallest element

---
+++ Heap/day02.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int kthSmallest(vector<int> arr, int k){
+    priority_queue<int> Heap;
+
+    for(int i = 0; i < k; i++){
+        Heap.push(arr[i]);
+    }
+
+    for(int i = k; i < arr.size(); i++){
+        if(arr[i] < Heap.top()){
+            Heap.pop();
+            Heap.push(arr[i]);
+        }
+    }
+
+    return Heap.top();
+
+}
+
+void kthSmallestFunction(){
+      vector<int> arr;
+    cout << "Enter the elements of array (enter -1 to stop) : ";
+    int temp;
+    cin >> temp;
+
+    while(temp != -1){
+        arr.push_back(temp);
+        cin >> temp;
+    }
+
+    int k;
+    cout << "Enter the value of k : ";
+    cin >> k;
+
+    if(k <= 0 || k > arr.size()){
+        cout << "Invalid k value. K must be between 1 and " << arr.size() << endl;
+        return ;
+    }
+
+    int answer = kthSmallest(arr, k);
+
+    cout << "Kth smallest element : " << answer << endl;
+}
+
+int main(){
+
+  kthSmallestFunction();
+
+    return 0;
+}
