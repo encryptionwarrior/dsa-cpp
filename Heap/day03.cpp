c330115262ca7fd970eaceebe08bb7f6befde883
creativeSandeepKumar <sdp4209@gmail.com>
2025-08-30 02:14:18 +0530

    find median completed

--- Heap/day03.cpp
+++ Heap/day03.cpp
@@ -329,10 +329,87 @@ void smallestRangeKList(){
 
 }
 
+
+int signum(int a, int b){
+    return (a == b) ? 0 : (a > b ? 1 : -1);
+}
+
+void callMedian(int element, priority_queue<int> &maxHeap, priority_queue<int, vector<int>, greater<int>> &minHeap, int &median){
+    switch(signum(maxHeap.size(), minHeap.size())){
+        case 0:
+            if(element > median){
+                minHeap.push(element);
+                median = minHeap.top();
+            } else {
+                maxHeap.push(element);
+                median = maxHeap.top();
+            }
+            break;
+
+            case 1:
+                if(element > median){
+                    minHeap.push(element);
+                } else {
+                    minHeap.push(maxHeap.top());
+                    maxHeap.pop();
+                    maxHeap.push(element);
+                }
+
+                median = (minHeap.top() + maxHeap.top())/2;
+                break;
+
+
+            case -1:
+                if(element > median){
+                    minHeap.push(maxHeap.top());
+                    maxHeap.pop();
+                    maxHeap.push(element);
+                } else {
+                    minHeap.push(maxHeap.top());
+                       minHeap.push(maxHeap.top());
+                    maxHeap.pop();
+                    maxHeap.push(element);
+                }
+
+                median = (minHeap.top() + maxHeap.top())/2;
+                break;
+    }
+}
+
+vector<int> findMedian(vector<int>&arr, int N){
+    vector<int> ans;
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int median = 0;
+
+    for(int i = 0; i < N; i++){
+        callMedian(arr[i], maxHeap, minHeap, median);
+        ans.push_back(median);
+    }
+    return ans;
+}
+
+void fnFindMedian(){
+    int N;
+    cout << "Enter the size of the array : ";
+    cin >> N;
+    vector<int> arr(N);
+
+    cout << "Enter the elements of array : ";
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    vector<int> ans = findMedian(arr, N);
+
+    cout << "Median of the stream (final median) : " << ans[ans.size() - 1] << endl;
+}
+
 int main(){
     // findKthLarhestSum();
     // fnMergeKSortedArr();
     // fnMergeKOsrtedList();
-    smallestRangeKList();
+    // smallestRangeKList();
+    fnFindMedian();
     return 0;
 }
