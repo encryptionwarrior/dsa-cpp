81f8adb7719b3ea60290f56fb9820a0ef9e892f5
creativeSandeepKumar <sdp4209@gmail.com>
2025-09-03 01:53:17 +0530

    adjacency list graph

--- graph/day01.cpp
+++ graph/day01.cpp
@@ -50,7 +50,57 @@ void adjcencyList(){
 
 }
 
+
+template <typename T>
+    class Graph2 {
+        public: 
+            unordered_map<T, list<T>> adj;
+
+            void addEdge(T u, T v, bool direction){
+                adj[u].push_back(v);
+
+                if(direction == false){
+                    adj[v].push_back(u);
+                }
+            }
+
+        void printAdjList(){
+            for(auto i : adj){
+                cout << i.first << " -> ";
+
+                for(auto j: i.second){
+                    cout << j << " ";
+                }
+                cout << endl;
+            }
+        }
+
+    };
+
+void adjListGen(){
+     int n, m;
+    cout << "Enter the number of nodes : ";
+    cin >> n;
+    cout << "Enter the number of edges : ";
+    cin >> m;
+
+    Graph2<int> G;
+
+    cout << "Enter edges (u v) : " << endl;
+
+    for(int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+        G.addEdge(u, v, 0);
+    }
+
+    cout << "\nAdjacency list: " << endl;
+    G.printAdjList();
+
+}
+
 int main(){
-    adjcencyList();
+    // adjcencyList();
+    adjListGen();
     return 0;
 }
