7034ba0d9c6ec560e19fd223b8b528ce198f1ee6
creativeSandeepKumar <sdp4209@gmail.com>
2025-09-04 01:23:18 +0530

    cycle detection using DFS completed

--- graph/day02.cpp
+++ graph/day02.cpp
@@ -150,10 +150,90 @@ void DFSTraversal(){
 
 }
 
+bool isCyclicBFS(unordered_map<int, list<int>>&adjList, unordered_map<int, bool> &visited, int Node){
+    unordered_map<int, int> parent;
+    parent[Node] = -1;
+
+    visited[Node] = true;
+
+    queue<int> Q;
+    Q.push(Node);
+
+    while(!Q.empty()){
+        int Frontval = Q.front();
+        Q.pop();
+
+        for(int neighbor: adjList[Frontval]){
+            if(visited[neighbor] == true && parent[Frontval] != neighbor){
+                return true;
+            }
+
+            if(visited[neighbor] == false){
+                visited[neighbor] = true;
+                parent[neighbor] = Frontval;
+                Q.push(neighbor);
+            }
+        }
+    }
+
+    return false;
+}
+
+string cycleDetection(vector<vector<int>> &edges, int n, int m){
+    unordered_map<int, list<int>> adjList;
+    unordered_map<int, bool> visited;
+
+    for(int i = 0; i < m; i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+    for(int i = 1; i <= n; i++){
+        if(visited[i] == false){
+            bool ans = isCyclicBFS(adjList, visited, i);
+
+            if(ans){
+                return "YES";
+            }
+        }
+    }
+
+    return "NO";
+}
+
+
+void cycleDetectionUsingBFS(){
+    int n, m;
+
+    cout << "Enter the number of nodes : ";
+    cin >> n;
+
+
+    cout << "Enter the number of edges : ";
+    cin >> m;
+
+    vector<vector<int>> edges;
+
+    cout << "Enter the edges (u, v) : " << endl;
+    for(int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+    edges.push_back({u, v});
+    }
+
+    string ans = cycleDetection(edges, n, m);
+
+    cout << "Cycle Detection Result : " << ans << endl;
+
+}
 
 
 int main(){
     // BFSTraversal();
-    DFSTraversal();
+    // DFSTraversal();
+    cycleDetectionUsingBFS();
     return 0;
 }
