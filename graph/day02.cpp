831606e4b7b36d3cf2d3c1e26fa47ef71862171c
creativeSandeepKumar <sdp4209@gmail.com>
2025-09-04 00:26:18 +0530

    bfs travsersal completed"

---
+++ graph/day02.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<queue>
+using namespace std;
+
+void traversal(unordered_map<int, list<int>> &adjList, unordered_map<int, bool> &visited, vector<int> &answer, int node){
+    queue<int> Q;
+
+    Q.push(node);
+
+    visited[node] = 1;
+
+    while(!Q.empty()){
+        int frontVal = Q.front();
+        Q.pop();
+
+        answer.push_back(frontVal);
+
+        for(auto neighbour: adjList[frontVal]){
+            if(!visited[neighbour]){
+                Q.push(neighbour);
+                visited[neighbour] = true;
+            }
+        }
+    }
+}
+
+
+vector<int> BFS(int vertex, vector<pair<int, int>> edges){
+    unordered_map<int, list<int>> adjList;
+    vector<int> answer;
+    unordered_map<int, bool> visited;
+
+    for(int i = 0; i < edges.size(); i++){
+        int u = edges[i].first;
+        int v = edges[i].second;
+
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+    for(int i = 0; i < vertex; i++){
+        if(!visited[i]){
+            traversal(adjList, visited, answer, i);
+        }
+    }
+
+    return answer;
+}
+
+void BFSTraversal(){
+    int n, m;
+
+    cout << "Enter the number of nodes : ";
+    cin >> n;
+
+
+    cout << "Enter the number of edges : ";
+    cin >> m;
+
+    vector<pair<int, int>> edges(m);
+
+    cout << "Enter the edges (u, v) : " << endl;
+    for(int i = 0; i < m; i++){
+        cin >> edges[i].first;
+        cin >> edges[i].second;
+    }
+
+    vector<int> solution = BFS(n, edges);
+
+    cout << "BFS Traversal" " ";
+
+    for(auto node : solution){
+        cout << node << " ";
+    }
+
+    cout << endl;
+}
+
+int main(){
+    BFSTraversal();
+    return 0;
+}
