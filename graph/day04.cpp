8617eae14c4b333813185fcfa46d6819e7c008a7
creativeSandeepKumar <sdp4209@gmail.com>
2025-09-06 00:31:39 +0530

    distanct joint compl,eted

--- graph/day04.cpp
+++ graph/day04.cpp
@@ -8,6 +8,7 @@
 #include<string>
 #include<algorithm>
 #include<limits.h>
+#include<set>
 using namespace std;
 
 vector<int> shortestPath(vector<pair<int, int>> &edges, int n, int m, int src, int tar) {
@@ -209,9 +210,153 @@ void shortestPathUsingDirectedAcyclic(){
     cout << endl;
 }
 
+class Graph {
+    public:
+        unordered_map<int, list<pair<int, int>>> adjList;
+
+        void addEdge(int u, int v, int weight){
+            adjList[u].push_back(make_pair(v, weight));
+        }
+
+        void printEdge(){
+
+            for(auto const& [node, neighbors]: adjList){
+                cout << node << " -> ";
+                for(auto const& neighbor_pair: neighbors){
+                    cout << neighbor_pair.first << "[" << neighbor_pair.second << "]";
+                }
+                cout << endl;
+            }
+        }
+
+        private: 
+            void solveDFS(stack<int> &st, vector<int> &visisted, int node){
+                visisted[node] = 1;
+
+                for(pair<int, int> neigh: adjList[node]){
+                    if(visisted[neigh.first] == 0){
+                        solveDFS(st, visisted, neigh.first);
+                    }
+                }
+
+                st.push(node);
+            }
+
+        public: 
+            stack<int> topoSort(){
+                stack<int> st;
+                vector<int> visited(6, 0);
+
+                for(int i = 0; i < 6; ++i){
+                    if(visited[i] == 0){
+                        if(adjList.count(i) || !adjList[i].empty() || i < 6){
+                            solveDFS(st, visited, i);
+                        }
+                    }
+                }
+                return st;
+            }
+
+            vector<int> shortestPath(int src){
+                vector<int> distance(6, INT_MAX);
+
+                distance[src] = 0;
+
+                stack<int> st = topoSort();
+
+                while(!st.empty()){
+                    int currentNode = st.top();
+                    st.pop();
+
+                    if(distance[currentNode] != INT_MAX){
+                        for(auto const& neighbor_pair: adjList[currentNode]){
+                            int neighbor = neighbor_pair.first;
+                            int weight = neighbor_pair.second;
+
+                            if(distance[currentNode] + weight < distance[neighbor]){
+                                distance[neighbor] = distance[currentNode] + weight;
+                            }
+                        }
+                    }
+                }
+                return distance;
+            }
+};
+
+void shorestPathDAGClass(){
+    Graph G;
+    G.addEdge(0, 1, 5);
+    G.addEdge(1, 2, 2);
+    G.addEdge(1, 3, 6);
+    G.addEdge(0, 2, 3);
+    G.addEdge(2, 3, 7);
+    G.addEdge(3, 4, -1);
+    G.addEdge(2, 4, 4);
+    G.addEdge(4, 5, -2);
+    G.addEdge(2, 5, 2);
+
+    int sourceNode = 1;
+    vector<int> shortestDistance = G.shortestPath(sourceNode);
+
+    cout << "Shortest distance from source code " << sourceNode << ":" << endl;
+
+    for(int i = 0; i < shortestDistance.size(); ++i){
+        cout << "Node" << i << ":" ;
+
+        if(shortestDistance[i] == INT_MAX){
+            cout << "INF";
+        } else {
+            cout << shortestDistance[i];
+        }
+
+        cout << endl;
+    }
+}
+
+
+vector<int> shorestPathDijkstras(vector<vector<int>> &edges, int n, int m, int src){
+    unordered_map<int, list<pair<int, int>>> addjList;
+
+    for(int i = 0; i < m; i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        int w = edges[i][2];
+
+        addjList[u].push_back({v, w});
+        addjList[v].push_back({v, w});
+    }
+
+    vector<int> distance(n, INT_MAX);
+
+    set<pair<int, int>> st;
+
+    distance[src] = 0;
+
+    st.insert({0, src});
+
+    while(!st.empty()){
+        pair<int, int> curr = *(st.begin());
+
+        int nodeDistance = curr.first;
+        int topNode = curr.second;
+
+        st.erase(st.begin());
+
+        for(auto neighbor_pair: addjList[topNode]){
+            int neighbor = neighbor_pair.first;
+            int edgeWeight = neighbor_pair.second;
+
+            if(nodeDistance + edgeWeight < distance[neighbor]){
+                auto record = st.find({distance[neighbor], neighbor});
+            }
+
+        }
+    }
+}
 
 int main(){
     // shortestPathUsingUnidirectional();
-    shortestPathUsingDirectedAcyclic();
+    // shortestPathUsingDirectedAcyclic();
+    shorestPathDAGClass();
     return 0;
 }
